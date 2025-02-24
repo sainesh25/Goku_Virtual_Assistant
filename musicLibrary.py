56d91cb6d297ebb0f0da9c51ad98cdb89278be45
sainesh25 <patilsainesh25@gmail.com>
2025-02-25 00:39:17 +0530

    music library added

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "labon": 'https://music.youtube.com/watch?v=12pMB_mCBOo',
+    'rahon': 'https://music.youtube.com/watch?v=55UJumsI1m0&list=RDAMVM55UJumsI1m0',
+    'aadat': 'https://music.youtube.com/watch?v=QGNcfBhGFdc'
+    
+}
