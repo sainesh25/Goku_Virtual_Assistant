9ebcc3da3714486781b4b1825e970b43623855d7
sainesh25 <patilsainesh25@gmail.com>
2025-02-25 00:38:41 +0530

    process command function created

--- main.py
+++ main.py
@@ -9,6 +9,24 @@ def speak(text):
     engin.say(text)
     engin.runAndWait()
 
+def processRequest(c):
+    if 'open google' in c.lower():
+        webbrowser.open('https://google.com')
+    elif 'open youtube' in c.lower():
+        webbrowser.open('https://youtube.com')
+    elif 'open my linkedin profile' in c.lower():
+        webbrowser.open('https://linkedin.com/in/sainesh-patil/')
+    elif 'open SP Gmail' in c.lower():
+        webbrowser.open('https://mail.google.com/mail/u/0/#inbox')
+    elif 'open whatsapp' in c.lower():
+        webbrowser.open('https://web.whatsapp.com/')
+    elif c.lower().startswith('play'):
+        song = c.lower().split(' ')[1]
+        link = musicLibrary.music[song]
+        print(link)
+        webbrowser.open(link)    
+    
+        
 if __name__ == "__main__":
     speak('Initializing Goku......')
     
